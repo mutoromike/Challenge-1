63fc490583e93744541c47c3bf551d58d670e167
mutoromike <mikemutoro@gmail.com>
2017-09-08 13:41:05 +0300

    FIX: bug on view.py

--- app_test.py
+++ app_test.py
@@ -6,7 +6,7 @@ class FirstTest(unittest.TestCase):
 
     def test_successful_create_new_user(self):
         result = {"mike":["mike@mike.com","123456"]}
-        msg = views.create_new_user('mike', '123456', 'mike@mike.com')
+        msg = views.create_new_user('mike', 'mike@mike.com', '123456')
         self.assertEqual(msg, result)
         
 if __name__ == '__main__':
