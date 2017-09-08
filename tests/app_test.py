1a101468e3a4de6d917bc6a65f8eb77477ba6ab9
mutoromike <mikemutoro@gmail.com>
2017-09-08 13:50:00 +0300

    FIX: on tests

--- tests/app_test.py
+++ tests/app_test.py
@@ -5,7 +5,7 @@ from app import views
 class FirstTest(unittest.TestCase):
 
     def test_successful_create_new_user(self):
-        result = {"mike":["mike@mike.com","123456"]}
+        result = {"mike":["123456","mike@mike.com"]}
         msg = views.create_new_user('mike', 'mike@mike.com', '123456')
         self.assertEqual(msg, result)
         
